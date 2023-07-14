8660c52b9f7af52296f3b6d07a4ed2c31368a934
caduong97 <duonghotinhca.97@gmail.com>
2023-07-14 11:26:56 +0300

    Add NAudio nuget package, BaseController and more project folders

---
+++ audio-modifier/Controllers/BaseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace audio_modifier.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseController : ControllerBase
+    {
+        private readonly ILogger<BaseController> _logger;
+        public IConfiguration _configuration;
+
+
+        public BaseController(ILogger<BaseController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+    }
+}
+
