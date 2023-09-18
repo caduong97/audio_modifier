84ab997c14ee8e4eba043d20f50f96ef890d9b14
caduong97 <duonghotinhca.97@gmail.com>
2023-09-19 00:20:24 +0300

    Small text adjustments to Home page

--- audio-modifier/ClientApp/src/pages/Home.tsx
+++ audio-modifier/ClientApp/src/pages/Home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <h1>Hello, world!</h1>
-      <p>Welcome to your new single-page application, built with:</p>
+      <p>Welcome to your simple audio_modifier app, built with:</p>
       <ul>
         <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
         <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
@@ -13,7 +13,7 @@ export default function Home() {
       </ul>
       <p>To help you get started, we have also set up:</p>
       <ul>
-        <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
+        <li><strong>Client-side navigation</strong>. For example, click <em>Converter</em> then <em>Back</em> to return here.</li>
         <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
         <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
       </ul>
