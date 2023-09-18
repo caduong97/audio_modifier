07f08d349fa64888abc8e4ce994fd172bf1dc4fd
caduong97 <duonghotinhca.97@gmail.com>
2023-09-19 00:20:00 +0300

    StereoToMono page: add convert button and dispatch stereoToMono action

--- audio-modifier/ClientApp/src/pages/SteoreoToMono.tsx
+++ audio-modifier/ClientApp/src/pages/SteoreoToMono.tsx
@@ -1,20 +1,27 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import UploadForm from "../components/uploads/UploadForm";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../store";
-import { audioMetadataRemoved, audioMetadatasCleared, audioMetadatasUpdated, preprocessAudio } from "../store/channelSlice";
+import { audioMetadataRemoved, audioMetadatasCleared, audioMetadatasUpdated, preprocessAudio, stereoToMono } from "../store/channelSlice";
 import { DndProvider } from "react-dnd";
 import UploadedList from "../components/uploads/UploadedList";
 import { TouchBackend } from "react-dnd-touch-backend";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { Button } from "reactstrap";
 import { AudioMetadataMp3, AudioMetadataWav } from "../models/AudioMetadata";
+import StereoToMonoRequest from "../models/channel/StereoToMonoRequest";
+import { v4 as uuidv4 } from 'uuid';
 
 export default function SteoreoToMono() {
   const [audioFiles, setAudioFiles] = useState<File[]>([])
   const dispatch: AppDispatch = useDispatch()
   const preprocessStatus = useSelector((state: RootState) => state.channel.preprocessStatus)
   const audioMetadatas = useSelector((state: RootState) => state.channel.audioMetadatas)
+
+  const [stereoToMonoRequest, setStereoToMonoRequest] = useState<StereoToMonoRequest>({
+    jobId: uuidv4(),
+    outputFileName: ""
+  })
 
   const isTouchDevice = () => {
     if ("ontouchstart" in window) {
@@ -51,6 +58,21 @@ export default function SteoreoToMono() {
       }
     })
   }, [audioMetadatas])
+
+  useEffect(() => {
+    if (audioFiles.length === 0 && audioMetadatas.length > 0) {
+      dispatch(audioMetadatasCleared())
+    }
+  }, [audioFiles])
+
+  const convertStereoToMono = useCallback(() => {
+    const formData = new FormData()
+    formData.append('files', audioFiles[0])
+
+    console.log("all files", formData.getAll("files"))
+    dispatch(stereoToMono({ form: formData, params: stereoToMonoRequest}))
+
+  }, [stereoToMonoRequest, audioFiles])
   
   return (
     <div>
@@ -82,6 +104,14 @@ export default function SteoreoToMono() {
               removeListItem={dispatchAudioRemove}
             />
           </DndProvider>
+
+          <div className="d-flex flex-column flex-sm-row justify-content-end mt-3">
+            <Button 
+              color="primary" 
+              className="ms-sm-3"
+              onClick={convertStereoToMono}
+            >Convert stereo to mono</Button>
+          </div>
         </>
       }
 
